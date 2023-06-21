6881f55ad4a365e76ba2e7f9b5cc7e8141bb4157
cooldragonkani <68496351+cooldragonkani@users.noreply.github.com>
2023-06-21 17:50:01 +0530

    Update calculateArea.js

--- calculateArea.js
+++ calculateArea.js
@@ -1,9 +1,10 @@
 function calculateArea(l,h)
 {
     var area = l*h;
-    console.log("Area of rectangle = ",a);
+    console.log("Area of rectangle = ",area);
 }
 
 var l,h;
 l = prompt("Enter lenght: ");
 h = prompt("Enter height: ");
+calculateArea(l,h);
